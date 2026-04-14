0cd1667ad996472e98fc95134ad159226f41881d
caKambourides <charalaboskambourides@gmail.com>
2026-04-14 08:42:13 +0300

    emacs treesit etc

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,3 +1,4 @@
+
 ;;gui cleanup
 (menu-bar-mode -1)
 (tool-bar-mode -1)
@@ -47,8 +48,8 @@
 (use-package doom-themes
   :ensure t
   :config
-  (load-theme 'doom-one t))
-;;transparency
+  (load-theme 'wombat t)) ;;set theme here
+;transparency
 (add-to-list 'default-frame-alist '(alpha-background . 95)) ; For all new frames henceforth
 
 ;; EVIL MODE
@@ -60,7 +61,22 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
-;;todo use evil collection for extra stuff
+;; enable mouse in tui
+(use-package clipetty
+  :ensure t
+  :hook (after-init . global-clipetty-mode))
+    ;; adjust scroll speed
+(setq mouse-wheel-scroll-amount
+      '(5
+        ((shift) . hscroll)
+        ((meta))
+        ((control meta) . global-text-scale)
+        ((control) . text-scale)))
+
+(add-hook 'evil-insert-state-entry-hook (lambda () (send-string-to-terminal "\033[5 q")))
+(add-hook 'evil-insert-state-exit-hook  (lambda () (send-string-to-terminal "\033[2 q")))
+
+    ;;todo use evil collection for extra stuff
 
 ;;todo god mode
 ;;(unless (package-installed-p 'god-mode)
@@ -71,80 +87,79 @@
 
 ;; tree sitter (built in?)
 ;;(unless (package-installed-p 'tree-sitter)
-;;(package-install 'tree-sitter))
-;;(require 'tree-sitter)
-;;(unless (package-installed-p 'tree-sitter-langs)
-;;(package-install 'tree-sitter-langs))
-;;(require 'tree-sitter-langs)
-;;(global-tree-sitter-mode)
-
-;;(unless (package-installed-p 'treesit)
-;;(package-install 'treesit))
-;;(require 'treesit)
-
-;;(unless (package-installed-p 'treesit-auto)
-;;(package-install 'treesit-auto))
-;;(use-package treesit-auto
-;;  :custom
-;;  (treesit-auto-install 'prompt)
-;;  :config
-;;  (treesit-auto-add-to-auto-mode-alist 'all)
-;;  (global-treesit-auto-mode))
+;(package-install 'tree-sitter))
+;(require 'tree-sitter)
+;(unless (package-installed-p 'tree-sitter-langs)
+;(package-install 'tree-sitter-langs))
+;(require 'tree-sitter-langs)
+;(global-tree-sitter-mode)
+;(use-package treesit-auto
+;  :ensure t
+;  :custom
+;  (treesit-auto-install 'prompt)
+;  :config
+;  (treesit-auto-add-to-auto-mode-alist 'all)
+;  (global-treesit-auto-mode))
 
 ;;By default, Emacs requires you to hit ESC three times to escape quit the minibuffer
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (rust "https://github.com/tree-sitter/tree-sitter-rust")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 (global-set-key [escape] 'keyboard-escape-quit)
 
 ;;todo lsp mode
-
-;;(unless (package-installed-p 'lsp-mode)
 ;;(package-install 'lsp-mode))
-;;(use-package lsp-mode
-;;  :init
+(use-package lsp-mode
+             :ensure t
+  :init
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  ;; (setq lsp-keymap-prefix "C-c l")
- ;; (setq lsp-auto-guess-root nil)
- ;; :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         ;;(XXX-mode . lsp)
-   ;;      (js-base-mode . lsp)
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (typescript-ts-mode . lsp-deferred)
+         (tsx-ts-mode . lsp-deferred)
+         (php-ts-mode . lsp)
+         (rust-ts-mode . lsp)
          ;; if you want which-key integration
-     ;;    (lsp-mode . lsp-enable-which-key-integration))
-  ;;:commands lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-tailwindcss
+    :ensure t
+  :after lsp-mode
+  :init
+  (setq lsp-tailwindcss-add-on-mode t))
 
 ;;errors
-;;(unless (package-installed-p 'flycheck)
-;;(package-install 'flycheck))
-;;(use-package flycheck
-;;  :ensure t
-;;  :config
-;;  (add-hook 'after-init-hook #'global-flycheck-mode))
+(unless (package-installed-p 'flycheck)
+(package-install 'flycheck))
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'after-init-hook #'global-flycheck-mode))
 
 ;; optionally
-;;(unless (package-installed-p 'lsp-ui)
-;;(package-install 'lsp-ui))
-;;(use-package lsp-ui :commands lsp-ui-mode)
-;; if you are helm user
-;;(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-
-;;(unless (package-installed-p 'lsp-ivy)
-;;(package-install 'lsp-ivy))
-;;(use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;;(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; optionally if you want to use debugger
-;;(use-package dap-mode)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-;; optional if you want which-key integration
-;;(use-package which-key
-;;    :config
-;;    (which-key-mode))
-
+(unless (package-installed-p 'lsp-ui)
+(package-install 'lsp-ui))
+(use-package lsp-ui :commands lsp-ui-mode)
 ;;alt eglot: install the lsp on your machine and configure it here 
 ;;(use-package eglot
 ;;  :hook ((typescript-ts-mode tsx-ts-mode js-ts-mode) . eglot-ensure))
 
-
+;;or disable
 
 ;; dashboard (startup)
 
@@ -268,51 +283,8 @@
 )
 
 ;;todo vterm
-(use-package vterm :ensure t
-:config
-(setq shell-file-name "/bin/sh"
-      vterm-max-scrollback 5000))
-(use-package vterm-toggle :ensure t
-  :after vterm
-  :config
-  ;; When running programs in Vterm and in 'normal' mode, make sure that ESC
-  ;; kills the program as it would in most standard terminal programs.
-  (evil-define-key 'normal vterm-mode-map (kbd "<escape>") 'vterm--self-insert)
-  (setq vterm-toggle-fullscreen-p nil)
-  (setq vterm-toggle-scope 'project)
-  (add-to-list 'display-buffer-alist
-               '((lambda (buffer-or-name _)
-                     (let ((buffer (get-buffer buffer-or-name)))
-                       (with-current-buffer buffer
-                         (or (equal major-mode 'vterm-mode)
-                             (string-prefix-p vterm-buffer-name (buffer-name buffer))))))
-                  (display-buffer-reuse-window display-buffer-at-bottom)
-                  ;;(display-buffer-reuse-window display-buffer-in-direction)
-                  ;;display-buffer-in-direction/direction/dedicated is added in emacs27
-                  ;;(direction . bottom)
-                  ;;(dedicated . t) ;dedicated is supported in emacs27
-                  (reusable-frames . visible)
-                  (window-height . 0.4))))
+
 ;;todo fonts
-(set-face-attribute 'default nil
-  :font "FiraCode"
-  :height 110
-  :weight 'medium)
-(set-face-attribute 'variable-pitch nil
-  :font "FiraCode"
-  :height 120
-  :weight 'medium)
-(set-face-attribute 'fixed-pitch nil
-  :font "FiraCode"
-  :height 110
-  :weight 'medium)
-;; Makes commented text and keywords italics.
-;; This is working in emacsclient but not emacs.
-;; Your font must have an italic face available.
-(set-face-attribute 'font-lock-comment-face nil
-  :slant 'italic)
-(set-face-attribute 'font-lock-keyword-face nil
-  :slant 'italic)
 
 ;; This sets the default font on all graphical frames created after restarting Emacs.
 ;; Does the same thing as 'set-face-attribute default' above, but emacsclient fonts
@@ -323,19 +295,47 @@
 (setq-default line-spacing 0.12)
 ;;sane defaults
 (delete-selection-mode 1)    ;; You can select text and delete it by typing.
-(electric-indent-mode -1)    ;; Turn off the weird indenting that Emacs does by default.
+;;(electric-indent-mode -1)    ;; Turn off the weird indenting that Emacs does by default.
+    (electric-indent-mode 1)
+            
+    
 (electric-pair-mode 1)       ;; Turns on automatic parens pairing
+(auto-save-mode -1)
 
 ;; Zoom in and out
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
 (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+(global-set-key (kbd "C-c t x") #'xterm-mouse-mode)
+
+(global-set-key (kbd "C-c t e") #'evil-mode)
+(global-set-key (kbd "C-c c") 'comment-line)
+   
+ ;;custom hotkeys
+(defun my/reload-config ()
+  (interactive)
+  (load-file "~/.config/emacs/init.el")
+  (ignore (elpaca-process-queues)))
+(global-set-key (kbd "C-c e r") #'my/reload-config)  
+
+(defun my/open-config ()
+  (interactive)
+  (find-file user-init-file))
+(global-set-key (kbd "C-c e e") #'my/open-config)
+
+
+;;sett load config open config etc
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
+     "d481904809c509641a1a1f1b1eb80b94c58c210145effc2631c1a7f2e4a2fdf4"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+     default))
  '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -344,12 +344,13 @@
  ;; If there is more than one, they won't work right.
  )
 ;;tab space
-;; Source - https://stackoverflow.com/a
-;; Posted by alcortes, modified by community. See post 'Timeline' for change history
-;; Retrieved 2026-01-10, License - CC BY-SA 2.5
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+;; Source - https://stackoverflow.com/a
+;; Posted by alcortes, modified by community. See post 'Timeline' for change history
+;; Retrieved 2026-01-10, License - CC BY-SA 2.5
+(setq indent-tabs-mode nil)
+(setq tab-width 4)
 (setq indent-line-function 'insert-tab)
+(setq typescript-ts-mode-indent-offset 4) ;; insanity
 
 
 ;;my stuff
@@ -397,4 +398,166 @@
  ;;       (insert-image (create-image svg-data 'svg t))
  ;;       (read-only-mode 1))
  ;;     (display-buffer buffer))))
+
+;;telescope
+;; orderless search
+(use-package orderless
+  :ensure t
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (completion-pcm-leading-wildcard t)) ;; Emacs 31: partial-completion behaves like substring
+;; Enable Vertico.
+(use-package vertico
+  :ensure t
+  :custom
+   (vertico-scroll-margin 0) ;; Different scroll margin
+   (vertico-count 20) ;; Show more candidates
+   (vertico-resize t) ;; Grow and shrink the Vertico minibuffer
+   (vertico-cycle t) ;; Enable cycling for `vertico-next/previous'
+  :init
+  (vertico-mode))
+;; CONSULT
+;; Example configuration for Consult
+(use-package consult
+    :ensure t
+  ;; Replace bindings. Lazily loaded by `use-package'.
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g r" . consult-grep-match)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))                ;; orig. previous-matching-history-element
+
+  ;; The :init configuration is always executed (Not lazy)
+  :init
+
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
+  (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+
+  ;; Configure other variables and modes in the :config section,
+  ;; after lazily loading the package.
+  :config
+
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key "M-.")
+  ;; (setq consult-preview-key '("S-<down>" "S-<up>"))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep consult-man
+   consult-bookmark consult-recent-file consult-xref
+   consult-source-bookmark consult-source-file-register
+   consult-source-recent-file consult-source-project-recent-file
+   ;; :preview-key "M-."
+   :preview-key '(:debounce 0.4 any))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; "C-+"
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (keymap-set consult-narrow-map (concat consult-narrow-key " ?") #'consult-narrow-help)
+)
+            
+;;git
+(use-package git-gutter
+  :ensure t
+  :config
+  (global-git-gutter-mode +1))
+;; todo
+;;(use-package magit
+;;  :ensure t)
+
+;;todo enable electric-indent-mode
+;; set that to tabs 4
+;; magit-blame
+;(electric-indent-mode 1)
+;(setq indent-tabs-mode nil)
+;(setq tab-width 4)
+;(editorconfig-mode 1)
+;;(setq indent-line-function 'insert-tab)
+
+    ;; yank highluight
+(require 'pulse)
+
+(defun my/pulse-yank-advice (&rest _)
+  (pulse-momentary-highlight-region (mark) (point)))
+
+;; Emacs yank
+(advice-add 'yank :after #'my/pulse-yank-advice)
+(advice-add 'yank-pop :after #'my/pulse-yank-advice)
+
+;; Evil yank
+(advice-add 'evil-yank :after #'my/pulse-yank-advice)
+(advice-add 'evil-yank-line :after #'my/pulse-yank-advice)
+
+;;     (use-package volatile-highlights
+;;  :ensure t
+;;  :config
+;;  (volatile-highlights-mode 1))
+    ;; to reload file do load_file enter enter
+;; TODO rest
+
+
+
 
